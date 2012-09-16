52b5faea0a65de64e4495c99f81a238297173d56
Chad Masso <chad.m.masso@gmail.com>
2012-09-16 18:45:35 -0500

    fixed issue

--- corset/management/commands/corset.py
+++ corset/management/commands/corset.py
@@ -4,17 +4,24 @@ from django.conf import settings
 from corset import JS_DIRS
 import requests, os, glob
 
+CLOSURE_URL = 'http://closure-compiler.appspot.com/compile'
+payload = {'compilation_level': 'SIMPLE_OPTIMIZATIONS', 'output_format': 'json', 'output_info': 'compiled_code'}
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        CLOSURE_URL = 'http://closure-compiler.appspot.com/compile'
-        payload = {'compilation_level': 'SIMPLE_OPTIMIZATIONS', 'output_format': 'json', 'output_info': 'compiled_code'}
-        for d in JS_DIRS:
-            src = os.path.join(d, "src")
-            build = os.path.join(d, "build")
-            for f in glob.glob(os.path.join(src, '*.js')):
-                payload['js_code'] = open(os.path.join(src, f), 'r').read()
-                r = requests.post(CLOSURE_URL, data = payload)
-                compiled_code = r.json['compiledCode']
-                build_file = open("%s.min.js" % os.path.join(build, os.path.splitext(os.path.basename(f))[0]), 'w')
-                build_file.write(compiled_code)
-                build_file.close()
+        JS_DIRS = []
+        for root, dirs, files in os.walk(settings.PROJECT_ROOT):
+            for d in dirs:
+                if d == "js":
+                    path = os.path.join(root, d)
+                    JS_DIRS.append(path)
+                    for d in path:
+                        src = os.path.join(d, "src")
+                        build = os.path.join(d, "build")
+                        for f in glob.glob(os.path.join(src, '*.js')):
+                            payload['js_code'] = open(os.path.join(src, f), 'r').read()
+                            r = requests.post(CLOSURE_URL, data = payload)
+                            compiled_code = r.json['compiledCode']
+                            build_file = open("%s.min.js" % os.path.join(build, os.path.splitext(os.path.basename(f))[0]), 'w')
+                            build_file.write(compiled_code)
+                            build_file.close()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name = 'corset',
     packages = ['corset', 'corset.management', 'corset.management.commands'],
     package_dir = {'management' : 'management/', 'commands' : 'management/commands/'},
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Django JS/CSS compiler',
     author = 'Chad Masso',
     author_email = 'chad.m.masso@gmail.com',
