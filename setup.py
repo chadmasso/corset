38aaa9b1634bd55ee42ab49ac420686f6e9ee842
Chad Masso <chad.m.masso@gmail.com>
2012-09-16 18:34:59 -0500

    initial commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 setup(
-    name = 'pencilthin',
-    packages = ['pencilthin', 'pencilthin.management', 'pencilthin.management.commands'],
+    name = 'corset',
+    packages = ['corset', 'corset.management', 'corset.management.commands'],
     package_dir = {'management' : 'management/', 'commands' : 'management/commands/'},
     version = '0.0.1',
-    description = 'Handlebars Template Compiler with Django template tag support',
+    description = 'Django JS/CSS compiler',
     author = 'Chad Masso',
     author_email = 'chad.m.masso@gmail.com',
     maintainer = 'Chad Masso',
